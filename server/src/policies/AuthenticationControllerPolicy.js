f0e1c4431d2a5551f5c02b8832e3e545b6c428e1
Mohammad Wahyu Hidayat <hmwahyu@yahoo.com>
2018-03-06 15:06:36 +0700

    Login Back End

--- server/src/policies/AuthenticationControllerPolicy.js
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -22,7 +22,7 @@ module.exports = {
           res.status(400).send({
             error: `The password provided failed to match the following rules:
               <br>
-              1. It must contain ONLY the following characters: lower case, upper case, numurics.
+              1. It must contain ONLY the following characters: lower case, upper case, numerics.
               <br>
               2. It must be at least 8 characters in length an not greater than 32 characters in length.
             `
