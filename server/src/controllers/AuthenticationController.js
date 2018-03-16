66bf2a8e5edde807575861ed49724afc51ff093a
Mohammad Wahyu Hidayat <hmwahyu@yahoo.com>
2018-03-16 11:23:28 +0700

    6 back end

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -13,7 +13,14 @@ module.exports = {
   async register (req, res) {
     try {
       const user = await User.create(req.body)
-      res.send(user.toJSON())
+      // res.send(user.toJSON())
+      const userJson = user.toJSON()
+      // console.log(userJson)
+
+      res.send({
+        user: userJson,
+        token: jwtSignUser(userJson)
+      })
     } catch (err) {
       res.status(400).send({
         error: 'This email account is already in use.'
